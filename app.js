3a0d16d899d357893c3e46ca2df7ca1d46995ff8
WIN-6LUN2D0CH7K\user <tofiqmvsm2003@gmail.com>
2023-12-07 13:12:23 +0400

    Avoiding Data sanitization adding xss-clean package and express-mongo-sanitize package

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit')
 const AppError = require('./utils/appError')
 const helmet = require('helmet')
-const globalErrorHandler = require('./controllers/errorController')
-
 const app = express();
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+
+const globalErrorHandler = require('./controllers/errorController')
 const toursRouter = require('./routes/tourRouter')
 const usersRouter = require('./routes/userRouter')
 
@@ -43,6 +45,15 @@ app.use((req, res, next) => {
 
 //Body parser, reading data from the body
 app.use(express.json({limit : '10kb'}));
+
+
+//Data sanitization against NoSql query injection
+app.use(mongoSanitize())
+
+
+//Data sanitization agains XSS
+app.use(xss())
+
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`))
